00b629f49aaecbe996e549ef48acae1373e27a09
junjun11023 <jjjunpersonal@gmail.com>
2022-03-10 22:19:52 +0800

    Initial Commit

---
+++ register/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  register
+//
+//  Created by chenhsiangchun on 2022/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
