2bebb9582482994e1160a6af8e0b26c245cc655d
junjun11023 <jjjunpersonal@gmail.com>
2022-03-14 13:50:24 +0800

    use data service for realtime database manipulation

--- register/DataService.swift
+++ register/DataService.swift
@@ -8,18 +8,129 @@
 import Foundation
 import Firebase
 
-class DataService {
-    static let dataService = DataService()
-   
-    private var user = Database.database().reference().child("users")
+enum FLAG {
+    case userAlreadyExist
+    case userCreateSuccess
+    case userCreateFailed
+    case userUpdateSuccess
+    case userUpdateFailed
+    case userValid
+    case userInvalid
+    case userUnknown
+}
 
-    var USER: DatabaseReference {
+
+class DataService {
+        
+    static let dataService = DataService()
+
+    private var USER: DatabaseReference {
         return Database.database().reference().child("users")
     }
 
 
-    func setUserInfo(user : User){
+    func setUserInfo(user: User){
         DataService.dataService.USER.child(user.username).child("email").setValue(user.email)
         DataService.dataService.USER.child(user.username).child("password").setValue(user.password)
+    }
+    
+    
+    func checkUserExists(user: User, completion: @escaping (FLAG)->() ) {
+        var exist = FLAG.userUnknown
+        if user.username.elementsEqual(Defaults.username) {
+            completion(exist)
+            return
+        }
+        
+  
+        DataService.dataService.USER.child(user.username).observeSingleEvent(of: .value, with: {(snapshot) in
+            print("checking")
+            if snapshot.exists() {
+                print("user exist")
+                exist = .userAlreadyExist
+                completion(exist)
+            } else {
+                print("user not exist")
+                exist = .userUnknown
+                completion(exist)
+            }
+            
+        })
+
+        return
+    }
+    
+    func createNewUser(user: User, completion: @escaping (FLAG)->() ) {
+        checkUserExists(user: user, completion: { flag in
+
+            switch flag {
+            case .userUnknown:
+                if !user.email.elementsEqual(Defaults.email), !user.username.elementsEqual(Defaults.username),
+                   !user.password.elementsEqual(Defaults.password) {
+                    
+                    let userInfo: [String: String] = [
+                        "email": user.email,
+                        "password": user.password
+                    ]
+                    DataService.dataService.USER.child(user.username).setValue(userInfo)
+                    completion(FLAG.userCreateSuccess)
+                }
+            default:
+                completion(FLAG.userCreateFailed)
+                print("not to create new user")
+            }
+        })
+
+    }
+    
+    
+    func updateUserInfo(user: User, completion: @escaping (FLAG)->() )  {
+        
+        checkUserExists(user: user, completion: { flag in
+            
+            switch flag {
+            case .userAlreadyExist:
+                if !user.password.elementsEqual(Defaults.password) {
+                    DataService.dataService.USER.child(user.username).updateChildValues(["password": user.password])
+                    completion(FLAG.userUpdateSuccess)
+                } else {
+                    completion(FLAG.userUpdateFailed)
+                }
+                
+                
+            default:
+                completion(FLAG.userUpdateFailed)
+                print("not to update user")
+            }
+        })
+
+    }
+    
+    
+    func checkUserValid(user: User, completion: @escaping (FLAG)->()) {
+        checkUserExists(user: user, completion: { flag in
+            
+            switch flag {
+            case .userAlreadyExist:
+                DataService.dataService.USER.child(user.username).observeSingleEvent(of: .value, with: {(snapshot) in
+                    let value = snapshot.value as! Dictionary<String, Any>
+                    let email = value["email"] as! String
+                    let password = value["password"] as! String
+                    print(value, email, password)
+                    if user.email.elementsEqual(email), user.password.elementsEqual(password) {
+                        completion(FLAG.userValid)
+                    } else {
+                        completion(FLAG.userInvalid)
+                    }
+                    
+                })
+                
+                
+            default:
+                completion(FLAG.userInvalid)
+                print("user invalid")
+            }
+        })
+
     }
 }
